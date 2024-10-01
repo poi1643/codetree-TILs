5a3aeceb9aeeccb09ca3f084da4e209c1893da82
Bamon <140365991+poi1643@users.noreply.github.com>
2024-10-02 03:54:00 +0900

    Title: 배열 놀이, Time: 134ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241002/배열 놀이/play-with-array.py
@@ -0,0 +1,29 @@
+n, q = map(int, input().split())
+
+arr = list(map(int, input().split()))
+
+
+for i in range(q):
+    question = list(map(int, input().split()))
+    if question[0] == 1:
+        print(arr[question[1]-1])
+        
+    elif question[0] == 2:
+        flag = 0
+        target = question[1]
+        for idx, j in enumerate(arr):
+            if target == j:
+                print(idx+1)
+                flag = 1
+                break
+        if flag == 0:
+            print(0)
+   
+
+
+            
+    if question[0] == 3:
+        l, m = question[1], question[2]
+        
+        for j in arr[question[1]-1:question[2]]:
+            print(j, end= ' ')
