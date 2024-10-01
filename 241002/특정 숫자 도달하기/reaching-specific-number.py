d1cb3af374bbdc82e6b83056325d5243b5255c36
Bamon <140365991+poi1643@users.noreply.github.com>
2024-10-02 01:43:23 +0900

    Title: 특정 숫자 도달하기, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241002/특정 숫자 도달하기/reaching-specific-number.py
@@ -0,0 +1,13 @@
+arr = list(map(int, input().split()))
+result = 0
+num = 0
+flag = False
+
+for i in arr:
+    if i<249:
+        result += i
+        num += 1
+    else:
+        break
+
+print(result, result/float(num))
