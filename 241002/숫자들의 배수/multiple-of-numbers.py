a54e3bb136ee898991f3f3928033c23dbcf2826a
Bamon <140365991+poi1643@users.noreply.github.com>
2024-10-02 02:53:44 +0900

    Title: 숫자들의 배수, Time: 105ms, Memory: 24MB, Status: Passed - CodeTree

--- 241002/숫자들의 배수/multiple-of-numbers.py
+++ 241002/숫자들의 배수/multiple-of-numbers.py
@@ -3,9 +3,8 @@ cnt = 1
 flag = 0
 while True:
     print(n * cnt, end =' ')
-    cnt += 1
     if n * cnt % 5 == 0:
-        flag += 1
-        if flag == 2:
-            print(n*cnt)
+        flag += 1 
+    if flag == 2:
             break
+    cnt += 1
