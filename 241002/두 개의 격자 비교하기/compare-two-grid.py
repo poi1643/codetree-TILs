55b4634a8cf931f48963cbd373f527f11cfdc95f
Bamon <140365991+poi1643@users.noreply.github.com>
2024-10-02 06:19:39 +0900

    Title: 두 개의 격자 비교하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 241002/두 개의 격자 비교하기/compare-two-grid.py
+++ 241002/두 개의 격자 비교하기/compare-two-grid.py
@@ -1,16 +1,14 @@
-n, m = map(int, input().split())  # n: 열의 크기, m: 행의 크기
+n, m = map(int, input().split())
 
-arr1 = [list(map(int, input().split())) for _ in range(m)]  # 첫 번째 배열 입력 받기
-arr2 = [list(map(int, input().split())) for _ in range(m)]  # 두 번째 배열 입력 받기
+arr1 = [list(map(int, input().split())) for _ in range(n)]
+arr2 = [list(map(int, input().split())) for _ in range(n)]
 
-result = [[1]*n for _ in range(m)]  # 결과 배열을 1로 초기화
+result = [[1]*n for _ in range(m)]
 
-# 배열 요소를 비교하여 동일하면 0으로 설정
-for i in range(m):  # 행의 크기 m 만큼 반복
-    for j in range(n):  # 열의 크기 n 만큼 반복
+for i in range(n):
+    for j in range(m):
         if arr1[i][j] == arr2[i][j]:
             result[i][j] = 0
 
-# 결과 출력
 for row in result:
     print(' '.join(map(str, row)))
