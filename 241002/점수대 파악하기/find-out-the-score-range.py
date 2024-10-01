408f10a21630db70b33fdd3bfc13086b30387805
Bamon <140365991+poi1643@users.noreply.github.com>
2024-10-02 03:17:12 +0900

    Title: 점수대 파악하기, Time: 120ms, Memory: 24MB, Status: Passed - CodeTree

--- 241002/점수대 파악하기/find-out-the-score-range.py
+++ 241002/점수대 파악하기/find-out-the-score-range.py
@@ -3,10 +3,12 @@ arr = list(map(int, input().split()))
 score = [0]*11
 
 for i in arr:
-    if arr == 0:
+    if i == 0:
         break;
-    else:
+    if i >= 10:
         score[i//10] += 1
+    else:
+        continue
 
 for i in range(10, 0, -1):
     print('%d - %d' %(i*10, score[i]))
