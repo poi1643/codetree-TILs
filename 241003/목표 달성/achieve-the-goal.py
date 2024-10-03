00e54c8186e0321ec8fb651e397486792d6ea1b1
Bamon <140365991+poi1643@users.noreply.github.com>
2024-10-03 12:33:15 +0900

    Title: 목표 달성, Time: 877ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 241003/목표 달성/achieve-the-goal.py
+++ 241003/목표 달성/achieve-the-goal.py
@@ -1,3 +1,4 @@
+'''
 import math
 n, a, b, c, d = map(int, input().split())
 
@@ -24,5 +25,29 @@ for i in range(1, n//a+1):
                     #print(i, j)
                     min_energy = energy
 
+
+print(min_energy)
+'''
+
+import math
+
+n, a, b, c, d = map(int, input().split())
+
+min_energy = math.inf
+
+# c 작업(더 큰 작업)을 최대한 많이 사용하고, 나머지를 a 작업으로 채우기
+for i in range(n // c + 1):
+    # 남은 상태
+    remaining_status = n - i * c
+    # 필요한 a 작업의 횟수
+    if remaining_status > 0:
+        a_work = math.ceil(remaining_status / a)
+    else:
+        a_work = 0
+    
+    # 총 에너지 계산
+    total_energy = i * d + a_work * b
+    # 최소 에너지 갱신
+    min_energy = min(min_energy, total_energy)
 
 print(min_energy)
