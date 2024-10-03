21ee59cac38120ae1360c67314343767638643c7
Bamon <140365991+poi1643@users.noreply.github.com>
2024-10-03 10:20:58 +0900

    Title: 2진수로 변환하기, Time: 126ms, Memory: 37MB, Status: Wrong Answer - CodeTree

--- 241003/2진수로 변환하기/convert-to-binary.py
+++ 241003/2진수로 변환하기/convert-to-binary.py
@@ -1,4 +1,13 @@
 n = int(input())
-n = str(bin(n))
-n = n.replace('0b','')
-print(n)
+answer = []
+
+while True:
+    if n < 2:
+        answer.append(1)
+        break
+    else:
+        answer.append(n%2)
+        n //= 2
+
+for i in answer[::-1]:
+    print(i,end='')
