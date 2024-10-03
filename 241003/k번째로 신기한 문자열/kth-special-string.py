4546603b4b79a230c31963b942066a659f06d438
Bamon <140365991+poi1643@users.noreply.github.com>
2024-10-03 09:20:05 +0900

    Title: k번째로 신기한 문자열, Time: 176ms, Memory: 24MB, Status: Passed - CodeTree

--- 241003/k번째로 신기한 문자열/kth-special-string.py
+++ 241003/k번째로 신기한 문자열/kth-special-string.py
@@ -1,3 +1,4 @@
+'''
 n, k, T = input().split()
 
 n = int(n)
@@ -11,3 +12,30 @@ for i in range(n):
         answer.append(q)
 
 print(sorted(answer)[k-1])
+'''        
+
+# 변수 선언 및 입력
+n, k, t = tuple(input().split())
+n, k = int(n), int(k)
+
+
+# a가 b로 시작하는지를 확인합니다.
+def starts_with(a, b):
+    # b의 길이가 더 길 수는 없습니다.
+    if len(a) < len(b):
+        return False
+    
+    # b의 길이만큼 살펴보며, a와 문자열이 완벽히 동일한지 확인합니다.
+    return a[:len(b)] == b
+
+
+words = []
+for _ in range(n):
+    word = input()
+    if starts_with(word, t):
+        words.append(word)
+
+# 정렬을 진행합니다.
+words.sort()
+
+print(words[k - 1])
