3d49596470459718e2a31edd5f4f9ba126af08e4
Bamon <140365991+poi1643@users.noreply.github.com>
2024-10-03 11:08:46 +0900

    Title: 왔다 갔던 구역 2, Time: 125ms, Memory: 24MB, Status: Passed - CodeTree

--- 241003/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 241003/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -1,4 +1,3 @@
-'''
 n = int(input())
 
 result = [0] * 300
@@ -7,44 +6,15 @@ for i in range(n):
     x, direction = input().split()
     x = int(x)
     if direction == 'L':
-        for i in range(1, x+1):
-            result[place - i] += 1
+        for i in range(x):
+            result[place - i -1] += 1
         place -= x
     else:
-        for i in range(1, x+1):
+        for i in range(x):
             result[place + i] += 1
         place += x
 
 
-ans = 0
-for i in result:
-    if i > 1:
-        ans += 1
-
-print(ans)
-'''
-
-n = int(input())
-
-result = [0] * 300  # 큰 배열 생성
-place = 150  # 시작 위치를 배열의 중간으로 설정
-
-for _ in range(n):
-    x, direction = input().split()  # 입력을 받음
-    x = int(x)
-    
-    if direction == 'L':
-        # 왼쪽으로 x칸 이동
-        for i in range(1, x + 1):  # 현재 위치 제외하고 이동
-            result[place - i] += 1
-        place -= x  # 최종적으로 이동한 위치 갱신
-    else:
-        # 오른쪽으로 x칸 이동
-        for i in range(1, x + 1):  # 현재 위치 제외하고 이동
-            result[place + i] += 1
-        place += x  # 최종적으로 이동한 위치 갱신
-
-# 두 번 이상 방문한 구간 계산
 ans = 0
 for i in result:
     if i > 1:
