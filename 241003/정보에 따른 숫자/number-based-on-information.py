d3d7c3a06ab9a9f5d22fe47043e7aaeb08b84ed4
Bamon <140365991+poi1643@users.noreply.github.com>
2024-10-03 14:12:57 +0900

    Title: 정보에 따른 숫자, Time: 5074ms, Memory: 29MB, Status: Time Limit Exceed - CodeTree

--- 241003/정보에 따른 숫자/number-based-on-information.py
+++ 241003/정보에 따른 숫자/number-based-on-information.py
@@ -1,57 +1,49 @@
+import bisect
+
+# 입력 처리
 N, A, B = map(int, input().split())
+S_positions = []
+NS_positions = []
 
-s_list = [0] * 10000
-NS_list = [0] * 10000
-
-for i in range(N):
-    q, n = input().split()
-    n = int(n) - 1
-    if q == 'S':
-        s_list[n] = 1
+# S와 NS의 위치를 저장
+for _ in range(N):
+    label, num = input().split()
+    num = int(num)
+    if label == 'S':
+        S_positions.append(num)
     else:
-        NS_list[n] = 1
+        NS_positions.append(num)
 
+# S와 NS의 위치를 정렬
+S_positions.sort()
+NS_positions.sort()
 
-result = [0] * 10000
+# 특정 위치에서 가장 가까운 위치를 이분 탐색으로 찾는 함수
+def find_closest_position(positions, target):
+    if not positions:
+        return float('inf')  # 해당 리스트가 비어있으면 무한대를 반환
+    # 이분 탐색으로 위치 찾기
+    idx = bisect.bisect_left(positions, target)
+    
+    # 현재 위치와 가까운 값을 찾음
+    closest = float('inf')
+    if idx < len(positions):
+        closest = positions[idx] - target
+    if idx > 0:
+        closest = min(closest, target - positions[idx - 1])
+    
+    return closest
 
-def find_lefts(num, cnt):
-    if s_list[num] == 1:
-        return cnt
-    elif num == A:
-        return 1000000000
-    else:
-        return find_lefts(num-1, cnt+1)
+# 결과 계산
+count = 0
+for num in range(A, B + 1):
+    # 가장 가까운 S와 NS의 위치 차이를 구함
+    s_distance = find_closest_position(S_positions, num)
+    ns_distance = find_closest_position(NS_positions, num)
+    
+    # 조건에 맞으면 카운트 증가
+    if s_distance <= ns_distance:
+        count += 1
 
-def find_rights(num, cnt):
-    if s_list[num] == 1:
-        return cnt
-    elif num == B:
-        return 1000000000
-    else:
-        return find_rights(num+1, cnt+1)
-
-
-def find_leftns(num, cnt):
-    if NS_list[num] == 1:
-        return cnt
-    elif num == A:
-        return 1000000000
-    else:
-        return find_leftns(num-1, cnt+1)
-
-def find_rightns(num, cnt):
-    if NS_list[num] == 1:
-        return cnt
-    elif num == B:
-        return 1000000000
-    else:
-        return find_rightns(num+1, cnt+1)
-
-
-ans = 0
-for i in range(A, B+1):
-    if min(find_lefts(i, 0), find_rights(i, 0)) >= min(find_leftns(i, 0), find_rightns(i, 0)):
-        ans += 1
-
-
-print(ans)
+# 결과 출력
+print(count)
