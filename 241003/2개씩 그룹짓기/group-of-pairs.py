4f3929ea6bbb0a3836485ac4dd37fcd6e63ddcab
Bamon <140365991+poi1643@users.noreply.github.com>
2024-10-03 09:11:46 +0900

    Title: 2개씩 그룹짓기, Time: 122ms, Memory: 25MB, Status: Passed - CodeTree

--- 241003/2개씩 그룹짓기/group-of-pairs.py
+++ 241003/2개씩 그룹짓기/group-of-pairs.py
@@ -4,11 +4,11 @@ arr = list(map(int, input().split()))
 num = len(arr)
 max = 0
 min = 2000
-for i in range(num):
-    for j in range(i+1, num):
-        temp = arr[i] + arr[j]
-        if max < temp:
-            max = temp
-    if max < min:
-        min = max
-print(min)
+arr.sort()
+for i in range(n):
+    temp = arr[i] + arr[-(i+1)]
+    if max < temp:
+        max = temp
+    
+
+print(max)
