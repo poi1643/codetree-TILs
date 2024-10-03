5cae632e620d346f118fce7b0ff593370edb8e3e
Bamon <140365991+poi1643@users.noreply.github.com>
2024-10-03 10:07:37 +0900

    Title: 요일 맞추기, Time: 101ms, Memory: 24MB, Status: Passed - CodeTree

--- 241003/요일 맞추기/guess-day-of-week.py
+++ 241003/요일 맞추기/guess-day-of-week.py
@@ -1,7 +1,7 @@
 m1, d1, m2, d2 = map(int, input().split())
 
 day = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+month = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 target1 = 0
 target2 = 0
@@ -15,9 +15,7 @@ for i in range(m1):
 target2 += d1
 
 
-num = target1 - target2-1
-
-
+num = target1 - target2
 
 print(day[num%7])
 '''
