ce156350dc0fcf734b1ba77ce6e7538b73205bb5
Bamon <140365991+poi1643@users.noreply.github.com>
2024-10-03 11:05:55 +0900

    Title: 왔다 갔던 구역 2, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241003/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 241003/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -1,3 +1,4 @@
+'''
 n = int(input())
 
 result = [0] * 300
@@ -14,6 +15,30 @@ for i in range(n):
             result[place + i] += 1
         place += x
 
+
+ans = 0
+for i in result:
+    if i > 1:
+        ans += 1
+
+print(ans)
+'''
+
+n = int(input())
+
+result = [0] * 300
+place = 150
+for _ in range(n):
+    x, direction = input().split()
+    x = int(x)
+    if direction == 'L':
+        for i in range(1, x + 1):  # 시작 위치는 중복되므로 1부터 시작
+            result[place - i] += 1
+        place -= x
+    else:
+        for i in range(1, x + 1):  # 시작 위치는 중복되므로 1부터 시작
+            result[place + i] += 1
+        place += x
 
 ans = 0
 for i in result:
