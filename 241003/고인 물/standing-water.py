52a4efe9606344efe315b92913517f696c596d2c
Bamon <140365991+poi1643@users.noreply.github.com>
2024-10-03 13:09:20 +0900

    Title: 고인 물, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 241003/고인 물/standing-water.py
@@ -0,0 +1,49 @@
+# m이 가로, n이 세로
+# r이 가로, c가 세로
+n, m = map(int, input().split())
+
+block = [[0]*m for _ in range(n)]
+arr = list(map(int, input().split()))
+
+
+#print(block)
+
+cnt = 0
+for i in arr:
+    for j in range(i):
+        block[j][cnt] = 1
+    cnt += 1
+    
+
+def find_left_wall(block, r, c):
+    if r == 0:
+        return False
+    else:
+        if block[c][r-1] == 1:
+            return True
+        else:
+            return find_left_wall(block, r-1, c)
+
+def find_right_wall(block, r, c):
+    if r == 5:
+        return False
+    else:
+        if block[c][r+1] == 1:
+            return True
+        else:
+            return find_left_wall(block, r+1, c)
+
+
+for i in range(m):
+    for j in range(n):
+        if block[j][i] != 1:
+            if find_left_wall(block, i, j) and find_right_wall(block, i, j):
+                block[j][i] = 2
+
+
+    
+
+ans = 0
+for i in block:
+    ans += i.count(2)
+print(ans)
