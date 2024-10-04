c18e911f846a4d50061153620da8e46bfa4976a2
Bamon <140365991+poi1643@users.noreply.github.com>
2024-10-05 07:26:34 +0900

    Title: 이상한 진수 2, Time: 126ms, Memory: 24MB, Status: Passed - CodeTree

--- 241005/이상한 진수 2/awkward-digits-2.py
+++ 241005/이상한 진수 2/awkward-digits-2.py
@@ -1,21 +1,19 @@
 n = input()
+
 if n == '1':
     print(0)
-elif '0' not in list(n):
-    print(list(n))
-    print(int(n,2) -1)
+elif '0' not in n:
+    print(int(n,2) - 1)
 
 else:
-
     n = list(n)
 
-    for i in range(1, len(n)):
+    for i in range(1, len(n)):  # 처음 만난 0을 1로 바꾸는 코드
         if n[i] == '0':
             n[i] = '1'
-        break
-
-    n = str(''.join(n))
-
-    n = int(n,2)
+            break  # 첫 번째 0을 1로 바꾼 후 반복 중단
+           
+    n = ''.join(n)
+    n = int(n, 2)
 
     print(n)
