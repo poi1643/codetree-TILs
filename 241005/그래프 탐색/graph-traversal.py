a72c66a85c38cbb346fbe71e7d75f707da7a07f0
Bamon <140365991+poi1643@users.noreply.github.com>
2024-10-05 08:22:48 +0900

    Title: 그래프 탐색, Time: 175ms, Memory: 26MB, Status: Passed - CodeTree

--- 241005/그래프 탐색/graph-traversal.py
+++ 241005/그래프 탐색/graph-traversal.py
@@ -1,11 +1,26 @@
 n, m = map(int, input().split())
 
-ans = [1]
+graph = [[] for _ in range(n+1)]
+visited = [0 for _ in range(n+1)]
+
+
+cnt = 0
+def dfs(index):
+    global cnt
+    for i in graph[index]:
+        if visited[i] == 0:
+            visited[i] = 1
+            cnt += 1
+            dfs(i)
+
+
 for i in range(m):
     a, b = map(int, input().split())
-    if a in ans and b not in ans:
-        ans.append(b)
-    elif b in ans and a not in ans:
-        ans.append(a)
+    graph[a].append(b)
+    graph[b].append(a)
 
-print(len(ans) - 1)
+
+visited[1] = 1
+dfs(1)
+
+print(cnt)
