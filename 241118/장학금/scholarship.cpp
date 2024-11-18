a847f7d87e629a3e7ec7e9d8eb9d49e0e964ae56
Bamon <140365991+poi1643@users.noreply.github.com>
2024-11-18 18:10:29 +0900

    Title: 장학금, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241118/장학금/scholarship.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b;
+    cin >> a >> b;
+    if(a < 90){
+        cout << "0";
+    }else if(b >= 95 ){
+        cout << "100000";
+    }else if(b >= 90 ){
+        cout << "50000";
+    }else{
+        cout << "0";
+    }
+    return 0;
+}
