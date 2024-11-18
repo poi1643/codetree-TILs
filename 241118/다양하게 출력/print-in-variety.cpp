6e79995ec8022ee6cb6bb842be9f3fe46246dbbb
Bamon <140365991+poi1643@users.noreply.github.com>
2024-11-18 14:54:08 +0900

    Title: 다양하게 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241118/다양하게 출력/print-in-variety.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cin.tie(0), cout.tie(0) , ios::sync_with_stdio(0);
+    cout << "Total days in Year\n365\nCircumference rate\n3.1415926535";
+    return 0;
+}
