d626089ee9e264f7d25333ddc69e691fd984499e
Bamon <140365991+poi1643@users.noreply.github.com>
2024-11-18 16:29:19 +0900

    Title: 정수 입력받아 계산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241118/정수 입력받아 계산/calculation-by-inputting-an-integer.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+    int a;
+    cin >> a;
+    cout << a*2+3;
+    return 0;
+}
