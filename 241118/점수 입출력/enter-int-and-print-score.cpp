533d70866a2d2b7b32c42cbfb92643c7af67ce7a
Bamon <140365991+poi1643@users.noreply.github.com>
2024-11-18 16:27:35 +0900

    Title: 점수 입출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241118/점수 입출력/enter-int-and-print-score.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+    int a;
+    cin >> a;
+    cout << "Your score is " << a << " point.";
+    return 0;
+}
