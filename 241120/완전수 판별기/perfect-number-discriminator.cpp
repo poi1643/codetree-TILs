e96defd17ad0e57722774a82a979238162125d66
Bamon <140365991+poi1643@users.noreply.github.com>
2024-11-20 00:00:48 +0900

    Title: 완전수 판별기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241120/완전수 판별기/perfect-number-discriminator.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n,sum = 0;
+    cin >> n;
+
+    for(int i = 1; i < n ; i++){
+        if(n%i == 0) sum += i;
+    }
+    if(sum == n) cout << 'P';
+    else cout << 'N';
+    return 0;
+}
