b791c4cf8da837fd33fe14102bf1947e55bd21c4
Bamon <140365991+poi1643@users.noreply.github.com>
2024-11-22 20:22:46 +0900

    Title: 알파벳 출력하기 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241122/알파벳 출력하기 2/print-alphabet-2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    char a = 'A'-1;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < i; j++) cout << "  ";
+        for(int j = 0; j < n - i; j++){ 
+            a = a + 1;
+            cout << a << ' ';
+        }
+        cout << '\n';
+    }
+    return 0;
+}
